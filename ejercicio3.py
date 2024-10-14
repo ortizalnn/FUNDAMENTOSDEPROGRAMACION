6098979a0a7570079c2a35d39e8d2a1d2129871a
ortizalnn <alanmontiel8671@gmail.com>
2024-10-14 10:29:15 -0600

    implementar match.sqrt en ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,21 @@
+'''
+ejercicio 3:
+ Programacion que calcula la hipotenusa a partir de los 
+ catetos
+ Entradas:
+ catetoA: int-> a
+ catetoB int -> b
+csalidas:
+hipotenusa: float -> c
+
+Analisis
+Se utiliza el teorema de pitagoras
+'''
+Import match
+a = input("Escribe el valor del cateto A: ")
+a = int(a)
+b = input("Escribe el valor del cateto B: ")
+b = int (b)
+c =  (a * a + b * b) ** (1/2)
+c = match.sqrt(a * a + b * b)
+print("El valor de la hipotenusa es:") , c
